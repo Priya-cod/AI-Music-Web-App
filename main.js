220b625ca24d1c08a66e430e1a0d17065bb9f4f5
Priya-cod <72642192+Priya-cod@users.noreply.github.com>
2021-05-27 14:01:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreleftWrist = 0;
+scorerightWrist = 0;
 
 function preload()
 {
@@ -29,6 +31,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
@@ -41,6 +46,20 @@ function gotPoses(results)
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreleftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        song1.play();
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        leftWristY_divide_1000 = remove_decimals/1000;
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 
 function play()
